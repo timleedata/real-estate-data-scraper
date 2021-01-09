67013f17d62b0b4e22617e34b5d653e3d92c538f
timleedata <timleedata@gmail.com>
2021-01-08 22:59:49 -0500

    add scrape listing details

--- scraper_driver.py
+++ scraper_driver.py
@@ -3,28 +3,33 @@ from scraper_helper import RedfinScraper
 
 # main
 def main():
-    # check arguments
-    args = sys.argv[1:]
-    args_len = len(args)
-    if args_len != 3:
-        print("Invalid arguments | Syntax: scraper_driver.py [ChromeDriver path] [location] [size]")
-        print("(E.g. %run ./scraper_driver.py \"C:\\Users\\ChromeDriver\\chromedriver.exe\" \"Atlanta, GA\" \"3000\""")")
+    try:
+        # check arguments
+        args = sys.argv[1:]
+        args_len = len(args)
+        if args_len != 3:
+            print("Invalid arguments | Syntax: scraper_driver.py [ChromeDriver path] [location] [size]")
+            print("(E.g. %run ./scraper_driver.py \"C:\\Users\\ChromeDriver\\chromedriver.exe\" \"Atlanta, GA\" \"3000\""")")
+            sys.exit(0)
+        
+        # argument variables
+        path = args[0]
+        loc = args[1]
+        size = args[2]
+    
+        # start data scraper
+        print("Starting web scrape task")
+        print("Note: If rs_df_tmp.csv is present in directory, it will be imported and possibly overwritten.\n\t- Please remove/rename this file to prevent this from happening.")
+        rs = RedfinScraper()
+        rs.run(path, loc, int(size))
+        
+        # gracefully exit
+        print("Completed web scrape task")
         sys.exit(0)
-    
-    # argument variables
-    path = args[0]
-    loc = args[1]
-    size = args[2]
-
-    # start data scraper
-    print("Starting web scrape task")
-    rs = RedfinScraper()
-    rs.run(path, loc, size)
-    
-    # gracefully exit
-    print("Completed web scrape task")
-    sys.exit(0)
-    
+    except Exception as e:
+        print("Failed to initiate Web scraper: {}".format(str(e)))
+        sys.exit(0)
+        
 if __name__ == "__main__":
     main()
     
--- scraper_helper.py
+++ scraper_helper.py
@@ -26,6 +26,15 @@ class RedfinScraper(object):
     # retuns href list
     def get_href_list(self) -> Set[str]:
         return self._href_list
+    
+    # load df from rs_df_tmp.csv
+    def load_csv_file(self) -> bool:
+        print("Method: load_csv_file()")
+        try:
+            self._df = pd.read_csv("rs_df_tmp.csv")
+            return True
+        except:
+            return False
 
     # close current webdriver
     def close_connection(self) -> bool:
@@ -85,7 +94,8 @@ class RedfinScraper(object):
             return self.check_listing_page()
         except:
             return False
-
+        
+    '''
     # checks whether current page is captcha
     def check_captcha(self) -> bool:
         print("Method: check_captcha()")
@@ -94,7 +104,8 @@ class RedfinScraper(object):
             return True
         except:
             return False
-
+    '''
+    
     # check if disabled in attributes
     def check_enabled(self, xpath: str)-> bool:
         print("Method: check_enabled()")
@@ -121,27 +132,60 @@ class RedfinScraper(object):
             return True
         except:
             return False
-    
-    # get details from listing cards
-    def scrape_listings(self, href_list: List[str]) -> bool:
-        print("Method: scrape_listings()")
+        
+    # export df to csv for future use
+    def export_df_to_csv(self):
+        print("Method: export_df_to_csv()")
         try:
-            for href in href_list:
-                self._driver.get(href)
-                if self.check_captcha():
-                    return True
-                else:
-                    print("Loaded listing card")
-                    neighborhood = self._driver.find_element_by_xpath("//div[contains(@class, 'ds-neighborhood')]//h4").get_attribute("innerHTML")
-                    walk_transit_scores = [el.get_attribute("innerHTML") for el in self._driver.find_elements_by_xpath("//a[@class='ws-value']//span")]
-                    
-                    print("Neighborhood: {} | Walk Score: {} | Transit Score: {}".format(neighborhood, walk_transit_scores[0], walk_transit_scores[1]))
+            self._df.to_csv("rs_df_tmp.csv")
+        except:
+            print("Failed to export df to rs_df_tmp.csv")
+        
+    # create df after listings retrieved
+    def create_and_save_df(self) -> bool:
+        print("Method: create_and_save_df()")
+        try:
+            self._df = pd.DataFrame(self._href_list, columns=["url"])
+            self._df["Checked"] = 0
+            self.export_df_to_csv()
             return True
         except:
+            print("Failed to create df")
             return False
+    
+    # get details from listing cards
+    def scrape_listings(self) -> bool:
+        print("Method: scrape_listings()")
+        try:
+            href_list = self._df[self._df['Checked'] == 0]['url'][0:3].tolist()
+            print(href_list)
+            for url in href_list:
+                self._driver.get(url)
+                street = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='street-address']"))).get_attribute("innerHTML")
+                city = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='locality']"))).get_attribute("innerHTML")
+                state = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='region']"))).get_attribute("innerHTML")
+                zip_code = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='postal-code']"))).get_attribute("innerHTML")
+                price = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='statsValue']/div/span[2]"))).get_attribute("innerHTML")
+                bed = WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='info-block']/div[@class='statsValue']")))[0].get_attribute("innerHTML")
+                bath = WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='info-block']/div[@class='statsValue']")))[1].get_attribute("innerHTML")
+                sqft = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='info-block sqft']//span[@class='statsValue']"))).get_attribute("innerHTML")
+                home_type = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='table-row' and ./span='Style']/div"))).get_attribute("innerHTML")
+                neighborhood = WebDriverWait(self._driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//h3[@class='h3 walkscore-header']"))).get_attribute("innerHTML")
+                walk = WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='percentage']/span[1]")))[0].get_attribute("innerHTML")
+                transit = WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='percentage']/span[1]")))[1].get_attribute("innerHTML")
+                bike = WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='percentage']/span[1]")))[2].get_attribute("innerHTML")
+                
+                #sold_prices = [el.get_attribute for el in WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='sold-row row PropertyHistoryEventRow']//div[@class='price-col number']"))).get_attribute("innerHTML")]
+                #sold_dates = [el.get_attribute for el in WebDriverWait(self._driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='sold-row row PropertyHistoryEventRow']/div[@class='col-4']/p[not(@class='subtext')]")))]
+                
+                print(f"street: {street}\ncity: {city}\nstate: {state}\nzip_code: {zip_code}\nprice: {price}\nbed: {bed}\nbath: {bath}\nsqft: {sqft}\nhome_type: {home_type}\nneighborhood: {neighborhood}\nwalk: {walk}\ntransit: {transit}\nbike: {bike})
+                
+            return False
+        except:
+            return True
 
     # run
-    def run(self, path: str, loc: str, size: str):
+    def run(self, path: str, loc: str, size: int):
         # set up proxy
         proxy_engine = ProxyEngine(path)
     
@@ -156,22 +200,18 @@ class RedfinScraper(object):
             options.add_argument("--start-maximized")
             self._driver = webdriver.Chrome(options=options, executable_path=path)
             try:
-                self._driver.get("https://www.redfin.com/")
-                if not self.check_captcha() and self.search_loc(loc):
-                    print("Successfully loaded listing page")
-                    self._size = int(size)
-                    if self.gather_hrefs():
-                        proxy_failed_or_blocked = False
-                        # temporary test
-                        df = pd.DataFrame(self._href_list, columns=['url'])
-                        df['Checked'] = 0
-                        
-                        print("Results:")
-                        print(df.head(10))
-                        print(df.info())
-                        print(len(df))
-                else:
-                    print("Proxy failed to load page")
+                self.load_csv_file()
+                if len(self._df) < size: # only retrieve listings if current df size
+                    self._driver.get("https://www.redfin.com/")
+                    if self.search_loc(loc):
+                        print("Successfully loaded listing page")
+                        self._size = size
+                        if self.gather_hrefs() and self.create_and_save_df():
+                            proxy_failed_or_blocked = self.scrape_listings()
+                    else:
+                        print("Proxy failed to load page")
+                else: # skip getting listings and scan current df
+                    proxy_failed_or_blocked = self.scrape_listings()
             except:
                 print("Proxy failed to load page")
             
