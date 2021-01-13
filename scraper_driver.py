998711ef6ef6a37bba7d7c7e1d3942ce952393a7
timleedata <timleedata@gmail.com>
2021-01-12 23:16:17 -0500

    add main functionalities

--- scraper_driver.py
+++ scraper_driver.py
@@ -1,5 +1,6 @@
+from funcs.scraper_helper import RedfinScraper
+from funcs.consts.constants import csv_file_name, loc_file_name
 import sys
-from scraper_helper import RedfinScraper
 
 # main
 def main():
@@ -7,27 +8,27 @@ def main():
         # check arguments
         args = sys.argv[1:]
         args_len = len(args)
-        if args_len != 3:
-            print("Invalid arguments | Syntax: scraper_driver.py [ChromeDriver path] [location] [size]")
-            print("(E.g. %run ./scraper_driver.py \"C:\\Users\\ChromeDriver\\chromedriver.exe\" \"Atlanta, GA\" \"3000\""")")
+        if args_len != 2:
+            print("Invalid arguments | Syntax: scraper_driver.py [ChromeDriver path] [location]")
+            print("(E.g. %run ./scraper_driver.py \"C:\\Users\\ChromeDriver\\chromedriver.exe\" \"Atlanta, GA\")")
+            print(f"Alternatively, specify \"import_loc_file\" as location to use data/{loc_file_name} to search multiple locations")
             sys.exit(0)
         
         # argument variables
         path = args[0]
         loc = args[1]
-        size = args[2]
     
         # start data scraper
         print("Starting web scrape task")
-        print("Note: If rs_df_tmp.csv is present in directory, it will be imported and possibly overwritten.\n\t- Please remove/rename this file to prevent this from happening.")
+        print(f"Note: If {csv_file_name} is present in data directory, it will be imported and possibly overwritten.\n\t- Please remove/rename this file to prevent this from happening.")
         rs = RedfinScraper()
-        rs.run(path, loc, int(size))
+        rs.run(path, loc)
         
         # gracefully exit
         print("Completed web scrape task")
         sys.exit(0)
     except Exception as e:
-        print("Failed to initiate Web scraper: {}".format(str(e)))
+        print(f"Failed to initiate Web scraper: {str(e)}")
         sys.exit(0)
         
 if __name__ == "__main__":
